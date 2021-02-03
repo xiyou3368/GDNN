01d3518d89acde6cb1d1ad999982da08efbf0c49
jiaguozi <18133496+xiyou3368@users.noreply.github.com>
2021-02-03 14:38:30 +0800

    Add files via upload

--- inits.py
+++ inits.py
@@ -2,6 +2,21 @@ import tensorflow as tf
 import numpy as np
 
 
+
+
+
+def weight_variable_glorot(input_dim, output_dim, name="",seed=123):
+    """Create a weight variable with Glorot & Bengio (AISTATS 2010)
+    initialization.
+    """
+    init_range = np.sqrt(6.0 / (input_dim + output_dim))
+    initial = tf.random_uniform([input_dim, output_dim], minval=-init_range,
+                                maxval=init_range,seed=seed, dtype=tf.float32)
+    return tf.Variable(initial, name=name)
+
+
+
+
 def uniform(shape, scale=0.05, name=None):
     """Uniform init."""
     initial = tf.random_uniform(shape, minval=-scale, maxval=scale, dtype=tf.float32)
